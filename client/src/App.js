a7f2210e02be2d51cc7be5fcdd226570dff0446e
Ethan <ethaneswanebban@gmail.com>
2025-02-09 22:02:37 +1100

    progress with frontend: auth page nearly done, need to connect to backend and and finalise button

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import { ThemeProvider, styled } from "styled-components";
+import { lightTheme } from "./utils/Themes";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Authentication from "./pages/Authentication";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+//import Navbar from "./components/Navbar";
+//import Dashboard from "./pages/Dashboard";
+//import Workouts from "./pages/Workouts";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`;
+
+function App() {
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+        <Container>
+          <Authentication />
+        </Container>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ client/src/components/SignUp.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+const Title = styled.div``;
+const Span = styled.div``;
+
+const SignUp = () => {
+  return (
+    <Container>
+      <div>
+        <Title>Welcome to Your Life</Title>
+        <Span>Please login with your details here</Span>
+      </div>
+      <div></div>
+    </Container>
+  );
+};
+
+export default SignUp;
